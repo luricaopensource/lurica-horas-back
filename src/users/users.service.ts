df2006d1d9f9c61b6d7ee6450297fda915ca575e
Julio Dechert <julitod55@hotmail.com>
2024-04-27 19:04:36 -0300

    Comment imports and decorators of user related functions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
-import { User } from './entities/user.entity'
-import { Repository } from 'typeorm'
-import { InjectRepository } from '@nestjs/typeorm'
+// import { User } from './entities/user.entity'
+// import { Repository } from 'typeorm'
+// import { InjectRepository } from '@nestjs/typeorm'
 
-@Injectable()
+// @Injectable()
 export class UsersService {
   constructor(
     // @InjectRepository(User)
