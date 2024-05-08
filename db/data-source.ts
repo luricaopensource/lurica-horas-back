08cbec59ed382789f0d701965ec2122e0343b5e3
Julio Dechert <julitod55@hotmail.com>
2024-05-08 18:39:49 -0300

    Change entities extension at DataSourceOptions

--- db/data-source.ts
+++ db/data-source.ts
@@ -10,7 +10,7 @@ export const dataSourceOptions: DataSourceOptions = {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: ['dist/**/*.entity.{js,ts}'],
+    entities: ['dist/**/*.entity.js'],
     migrations: ['dist/db/migrations/*.js'],
 }
 
