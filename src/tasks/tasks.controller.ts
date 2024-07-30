ef4556bfa279ae4bb5363da57e5413fd61d4830b
alexisrivero <alexisr15494@gmail.com>
2024-07-30 19:36:47 -0300

    added findAllByEmployee in task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -17,6 +17,11 @@ export class TasksController {
     return this.tasksService.findAll()
   }
 
+  @Get()
+  findAllByEmployee(@Param('employeeId') employeeId: string) {
+    return this.tasksService.findAllByEmployee(+employeeId);
+  }
+
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.tasksService.findOne(+id)
