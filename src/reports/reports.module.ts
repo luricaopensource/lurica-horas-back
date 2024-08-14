04e72092ba2befb31302e3ec5e903ed34a758a88
Julito01 <julitod55@hotmail.com>
2024-08-14 16:37:10 -0300

    Add milestone filter to generate reports

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -6,10 +6,11 @@ import { ProjectsModule } from 'src/projects/projects.module'
 import { UsersModule } from 'src/users/users.module'
 import { ClientModule } from 'src/client/client.module'
 import { TasksModule } from 'src/tasks/tasks.module'
+import { MilestoneModule } from 'src/milestone/milestone.module'
 
 @Module({
     controllers: [ReportsController],
     providers: [ReportsService],
-    imports: [PrinterModule, ProjectsModule, UsersModule, ClientModule, TasksModule]
+    imports: [PrinterModule, ProjectsModule, UsersModule, ClientModule, TasksModule, MilestoneModule]
 })
 export class ReportsModule { }
